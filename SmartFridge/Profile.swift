4e381253c047f98bdada5a4eeceff9dbdacc44cb
divyankithaRaghavaUrs <divyankitha>
2017-11-29 19:19:42 -0800

    User Profile - Divyankitha

--- SmartFridge/Profile.swift
+++ SmartFridge/Profile.swift
@@ -18,10 +18,22 @@ class Profile: UIViewController {
     @IBOutlet weak var SaveButton: UIButton!
     
     @IBOutlet weak var scrollView: UIScrollView!
+    
+    var EmailId = String()
+    var ContactNo = String()
+    var Address = String()
+    var Username = String()
+    var Lastname = String()
+    var FirstName = String()
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
         SaveButton.setTitleColor(UIColor.gray, for: .disabled)
+        
+        displayProfile()
+        
+        
     }
 
     override func didReceiveMemoryWarning()
@@ -30,6 +42,97 @@ class Profile: UIViewController {
         
     }
     
+    override func viewDidAppear(_ animated: Bool)
+    {
+        
+        Fname.text = FirstName
+        Lname.text = Lastname
+        Email.text = EmailId
+        Uname.text = Username
+    }
+    
+    
+    func displayProfile()
+    {
+        print("Inside fetch list")
+        
+        let DNS = RestApiUrl()
+        
+        var request = URLRequest(url: URL(string: DNS.aws + "/SmartFridgeBackend/user/profileDetails/2")!)
+        request.httpMethod = "GET"
+        request.httpBody = try? JSONSerialization.data(withJSONObject: [] ,options: [])
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let sessionConfig = URLSessionConfiguration.default
+        sessionConfig.timeoutIntervalForRequest = 60.0
+        sessionConfig.timeoutIntervalForResource = 60.0
+        
+        let session = URLSession(configuration: sessionConfig)
+        let task = session.dataTask(with: request, completionHandler: { data, response, error -> Void in print( response ?? "Error connecting to Rest API - GET user profile")
+            
+            if error != nil
+            {
+                print("Failed to connect to get user profile API")
+                print(error!)
+            }
+            else
+            {
+                print("Data Obtained")
+                
+                self.parseJSON(data!)
+            }
+            
+            if let httpResponse = response as? HTTPURLResponse
+            {
+                if httpResponse.statusCode == 200
+                {
+                    print("Retrived profile")
+                    
+                }
+                else
+                {
+                    print(httpResponse.statusCode)
+                    print("Failed to retrive profile")
+                }
+            }
+        })
+        
+        task.resume()
+        
+    }
+    
+    func parseJSON(_ data:Data)
+    {
+        
+        var jsonResult = NSDictionary()
+        
+        do{
+            
+            jsonResult = try JSONSerialization.jsonObject(with: data, options:JSONSerialization.ReadingOptions.allowFragments) as! NSDictionary
+            print(jsonResult)
+            
+        } catch let error as NSError
+        {
+            print(error)
+        }
+        
+        let E = jsonResult["EmailId"] as? String
+        //let C = jsonResult["ContactNo"] as? String
+        //let A = jsonResult["Address"] as? String
+        let U = jsonResult["Username"] as? String
+        let L = jsonResult["LastName"] as? String
+        let F = jsonResult["FirstName"] as? String
+        
+        EmailId = E!
+        //ContactNo = C!
+        //Address = A!
+        Username = U!
+        Lastname = L!
+        FirstName = F!
+        
+    }
+        
+        
     @IBAction func Edit(_ sender: UIButton)
     {
         Fname.isUserInteractionEnabled = true
