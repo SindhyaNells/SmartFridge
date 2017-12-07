e727c2583aa66734f467f1730b7dca3d915a23dc
sindhya <sindhu@Sindhus-MacBook-Pro.local>
2017-12-07 15:16:24 -0800

    Location

--- SmartFridge/LocationController.swift
+++ SmartFridge/LocationController.swift
@@ -9,26 +9,81 @@
 import UIKit
 import MapKit
 
-class LocationController: UIViewController {
+class LocationController: UIViewController,MKMapViewDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
     var groceryPlaces = Array<GroceryPlaces>()
     
+    fileprivate var isCurrentLocation: Bool = false
+    
+    var locationManager = CLLocationManager()
+    fileprivate var annotation: MKAnnotation!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        mapView.delegate = self
+        mapView.mapType = .standard
+        
+        if (CLLocationManager.locationServicesEnabled()) {
+            if locationManager == nil {
+                locationManager = CLLocationManager()
+            }
+            locationManager.requestAlwaysAuthorization()
+            locationManager.delegate = self as? CLLocationManagerDelegate
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.requestAlwaysAuthorization()
+            locationManager.startUpdatingLocation()
+            isCurrentLocation = true
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
+        getNearbyGroceryPlaces();
+    
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        if !isCurrentLocation {
+            return
+        }
+        
+        isCurrentLocation = false
+        
+        let location = locations.last
+        
+        let center = CLLocationCoordinate2D(latitude: location!.coordinate.latitude, longitude: location!.coordinate.longitude)
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        
+        self.mapView.setRegion(region, animated: true)
+        
+        if self.mapView.annotations.count != 0 {
+            annotation = self.mapView.annotations[0]
+            self.mapView.removeAnnotation(annotation)
+        }
+        
+        let pointAnnotation = MKPointAnnotation()
+        pointAnnotation.coordinate = location!.coordinate
+        pointAnnotation.title = ""
+        mapView.addAnnotation(pointAnnotation)
+    }
+    
+    func getNearbyGroceryPlaces(){
         let placesURL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=37.3352,-121.8811&radius=2000&type=grocery_or_supermarket&key=AIzaSyCQ7nzuqrkbFNf15vueqVljJixWo56jzRw"
         
         var request = URLRequest(url: URL(string: placesURL)!)
         request.httpMethod = "GET"
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-
+        
         let session = URLSession.shared
         
         let task = session.dataTask(with: request, completionHandler: { data, response, error -> Void in print( response ?? "Error connecting to Rest API - GET Google places")
@@ -61,13 +116,6 @@ class LocationController: UIViewController {
         })
         
         task.resume()
-    
-        
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     func parseJSON(_ data:Data){
