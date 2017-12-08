00c924000f40f5258fbf6f4513f9dcda4398abf1
divyankithaRaghavaUrs <divyankitha>
2017-12-08 12:48:22 -0800

    Added Pie chart

---
+++ SmartFridge/IngredientsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  IngredientsTableViewCell.swift
+//  SmartFridge
+//
+//  Created by Divyankitha Raghava Urs on 12/8/17.
+//  Copyright © 2017 SJSU. All rights reserved.
+//
+
+import UIKit
+
+class IngredientsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
