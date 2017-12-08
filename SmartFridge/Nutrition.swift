56809edeea48e661bcb4f095bed87c72b366f822
sindhya <sindhu@Sindhus-MBP.T-mobile.com>
2017-12-08 06:26:46 -0800

    nutrients changes

--- SmartFridge/Nutrition.swift
+++ SmartFridge/Nutrition.swift
@@ -13,6 +13,8 @@ class Nutrition: UIViewController
     var recipeName = String()
     var recipeID = String()
     //var recipeID = "1e5aa65869d8d215c78dd9720147f434"
+    var nutrients = [String:Double] ()
+    var calories = Double()
 
     override func viewDidLoad()
     {
@@ -156,47 +158,48 @@ class Nutrition: UIViewController
             json = try JSONSerialization.jsonObject(with: data) as? [Any]
             print(json ?? "error json")
              guard let item = json?.first as? [String: Any],
-              let calories = item["calories"] as? Double else{
+                let cal = item["calories"] as? Double,
+                let nutrients = item["totalNutrients"] as? [String: Any],
+                let fat = nutrients["FAT"] as? [String: Any],
+                let fat_quantity = fat["quantity"] as? Double,
+            let chole = nutrients["CHOLE"] as? [String: Any],
+            let choles_quantity = chole["quantity"] as? Double,
+            let carbs = nutrients["CHOCDF"] as? [String:Any],
+            let carbs_quantity = carbs["quantity"] as? Double,
+                let fiber = nutrients["FIBTG"] as? [String:Any],
+                let fiber_quantity = fiber["quantity"] as? Double,
+                let sugar = nutrients["SUGAR"] as? [String:Any],
+                let sugar_quantity = sugar["quantity"] as? Double,
+                let protein = nutrients["PROCNT"] as? [String:Any],
+                let protein_quantity = protein["quantity"] as? Double,
+                let vita = nutrients["VITA_RAE"] as? [String:Any],
+                let vita_quantity = vita["quantity"] as? Double,
+                let vitc = nutrients["VITC"] as? [String:Any],
+                let vitc_quantity = vitc["quantity"] as? Double
+                else{
             return
             }
-            print(calories)
-            /*if let json = try JSONSerialization.jsonObject(with: data) as? [String: Any],
-                let results = json as? [[String: Any]] {
-                for res in results {
-                    let calories = res["calories"] as! String
-                    //let placeName = res["name"] as! String
-                    //let googleLoc = googleGeo["location"] as! NSDictionary
-                    //let latitude = googleLoc["lat"] as! Float
-                    //let longitude = googleLoc["lng"] as! Float
-                    print(calories)
-                }
-            }*/
+            
+            self.calories = cal
+            self.nutrients["fat"] = fat_quantity
+            self.nutrients["cholestrol"] = choles_quantity
+            self.nutrients["carbs"] = carbs_quantity
+            self.nutrients["fiber"] = fiber_quantity
+            self.nutrients["sugar"] = sugar_quantity
+            self.nutrients["protein"] = protein_quantity
+            self.nutrients["vitamina"] = vita_quantity
+            self.nutrients["vitaminc"] = vitc_quantity
+            
+            //loop over nutrients
+            for (key, value) in self.nutrients {
+                print("\(key): \(value)")
+            }
+            
         } catch {
             print("Error deserializing JSON: \(error)")
         }
         
-        /*
-        var jsonElement = NSDictionary()
-        for i in 0 ..< jsonResult.count
-        {
-            
-            jsonElement = jsonResult[i] as! NSDictionary
-            
-            let GroceryItem = GroceryItemModel()
-            
-            let id = jsonElement["ListID"] as? Int
-            let uid = jsonElement["UserID"] as? Int
-            let name = jsonElement["FoodItemName"] as? String
-            let type = jsonElement["Type"] as? String
-            
-            print(id ?? 0)
-            
-            GroceryItem.name = name
-            GroceryItem.id = id
-            GroceryItem.uid = uid
-            GroceryItem.type = type
-            
-        }*/
+    
         
     }
     
