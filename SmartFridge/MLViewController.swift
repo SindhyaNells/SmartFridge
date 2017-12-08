a5630e035b1fc76503e8aa0a7a6eb358029b6b0d
sindhya <sindhu@Sindhus-MacBook-Pro.local>
2017-12-07 20:28:07 -0800

    Added CoreML

--- SmartFridge/MLViewController.swift
+++ SmartFridge/MLViewController.swift
@@ -7,9 +7,18 @@
 //
 
 import UIKit
+import CoreML
+import Vision
+import ImageIO
 
 class MLViewController: UIViewController {
 
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var arrowBtn: UIButton!
+    @IBOutlet weak var cameraButton: UIButton!
+    @IBOutlet weak var classificationLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,7 +30,101 @@ class MLViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    
+    lazy var classificationRequest: VNCoreMLRequest = {
+        do {
+            
+            let model = try VNCoreMLModel(for: Food101().model)
+            
+            let request = VNCoreMLRequest(model: model, completionHandler: { [weak self] request, error in
+                self?.processClassifications(for: request, error: error)
+            })
+            request.imageCropAndScaleOption = .centerCrop
+            return request
+        } catch {
+            fatalError("Failed to load Vision ML model: \(error)")
+        }
+    }()
+    
+    
+    func updateClassifications(for image: UIImage) {
+        classificationLabel.text = "Classifying..."
+        
+        let orientation = CGImagePropertyOrientation(image.imageOrientation)
+        guard let ciImage = CIImage(image: image) else { fatalError("Unable to create \(CIImage.self) from \(image).") }
+        
+        DispatchQueue.global(qos: .userInitiated).async {
+            let handler = VNImageRequestHandler(ciImage: ciImage, orientation: orientation)
+            do {
+                try handler.perform([self.classificationRequest])
+            } catch {
+                /*
+                 This handler catches general image processing errors. The `classificationRequest`'s
+                 completion handler `processClassifications(_:error:)` catches errors specific
+                 to processing that request.
+                 */
+                print("Failed to perform classification.\n\(error.localizedDescription)")
+            }
+        }
+    }
+    
+    /// Updates the UI with the results of the classification.
+    /// - Tag: ProcessClassifications
+    
+    func processClassifications(for request: VNRequest, error: Error?) {
+        DispatchQueue.main.async {
+            guard let results = request.results else {
+                //self.classificationRequest.text = "Unable to classify image.\n\(error!.localizedDescription)"
+                return
+            }
+            // The `results` will always be `VNClassificationObservation`s, as specified by the Core ML model in this project.
+            let classifications = results as! [VNClassificationObservation]
+            
+            if classifications.isEmpty {
+                //self.classificationRequest.text = "Nothing recognized."
+            } else {
+                // Display top classifications ranked by confidence in the UI.
+                let topClassifications = classifications.prefix(1)
+                let descriptions = topClassifications.map { classification in
+                    //return String(format: "  (%.2f) %@", classification.confidence, classification.identifier)
+                    return String(classification.identifier)
+                }
+                self.classificationLabel.text = "Food Identified as : " + descriptions.joined(separator: "\n")
+                print(descriptions[0])
+            }
+        }
+    }
+    
+    @IBAction func takePicture(_ sender: Any) {
+        
+        guard UIImagePickerController.isSourceTypeAvailable(.camera) else {
+            presentPhotoPicker(sourceType: .photoLibrary)
+            return
+        }
+        
+        let photoSourcePicker = UIAlertController()
+        let takePhoto = UIAlertAction(title: "Take Photo", style: .default) { [unowned self] _ in
+            self.presentPhotoPicker(sourceType: .camera)
+        }
+        let choosePhoto = UIAlertAction(title: "Choose Photo", style: .default) { [unowned self] _ in
+            self.presentPhotoPicker(sourceType: .photoLibrary)
+        }
+        
+        photoSourcePicker.addAction(takePhoto)
+        photoSourcePicker.addAction(choosePhoto)
+        photoSourcePicker.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        present(photoSourcePicker, animated: true)
+        
+    }
+    
+    func presentPhotoPicker(sourceType: UIImagePickerControllerSourceType) {
+        let picker = UIImagePickerController()
+        picker.delegate = self as UIImagePickerControllerDelegate & UINavigationControllerDelegate
+        picker.sourceType = sourceType
+        present(picker, animated: true)
+    }
+    
     /*
     // MARK: - Navigation
 
@@ -31,5 +134,21 @@ class MLViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    
+    
 
+}
+
+extension MLViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    // MARK: - Handling Image Picker Selection
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String: Any]) {
+        picker.dismiss(animated: true)
+        
+        // We always expect `imagePickerController(:didFinishPickingMediaWithInfo:)` to supply the original image.
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        imageView.image = image
+        updateClassifications(for: image)
+    }
 }
